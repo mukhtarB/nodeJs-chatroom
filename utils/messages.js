e31685e2d24407ef8f1c6237a0a52b7ca4e725b0
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-08-15 18:41:57 +0100

    Bug Fix: Fixed the module not found err

--- utils/messages.js
+++ utils/messages.js
@@ -2,8 +2,8 @@ const moment = require("moment");
 
 function formatMessage(username, text) {
     return {
-        username = username,
-        text = text,
+        username,
+        text,
         time: moment.format('h:mm a')
     }
 };
