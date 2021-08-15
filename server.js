ba95cf5cfaf02a50d0315dcf963dc977eb2cef32
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-08-15 04:10:41 +0100

    feat: Successfully established socket.io connection

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 // Spinning up the express server
 
-// initiaiize express
+// bringing in the needed modules
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
+// initializing variables
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Serving static files: Set static folder
 app.use(express.static(path.join('public')));
 
+
+// Run when a client connects to web socket
+io.on('connection', socket => {
+    console.log('New Web Socket (socket.io connection)');
+});
+
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
